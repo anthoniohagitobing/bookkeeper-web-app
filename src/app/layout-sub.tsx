0792e5aceaba2e213310bd50eef12652be0e35a8
Anthonio Hagi Tobing <133083562+anthoniohagitobing@users.noreply.github.com>
2024-01-04 20:34:53 +0900

    feat: context variable setup

---
+++ src/app/layout-sub.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+// IMPORT MODULES
+import { ContextVariables } from '../context-variables';
+import { useState } from 'react';
+import { lusitana } from '@/fonts';
+//import ResponsiveAppBar from './responsive-app-bar';
+// import Navbar from './navbar';
+
+// DATA TYPE
+interface Props {
+    childrenProp: React.ReactNode;
+}
+
+// PAGE COMPONENT
+export default function LayoutSub(props: Props): JSX.Element {
+    const [userId, setUserId] = useState<number>(0);
+    const [userUid, setUserUid] = useState<string>('noUid');
+    const [userFirstName, setUserFirstName] = useState<string>('noFirstName');
+    const [userLastName, setUserLastName] = useState<string>('noLastName');
+    const [userEmail, setUserEmail] = useState<string>('noEmail');
+    const [userPhotoUrl, setUserPhotoUrl] = useState<string>('noPhotoUrl');
+
+    return (
+        <ContextVariables.Provider value={{userId, userUid, userFirstName, userLastName, userEmail, userPhotoUrl, setUserId, setUserUid, setUserFirstName, setUserLastName, setUserEmail, setUserPhotoUrl}}>
+            <body className={`${lusitana.className}`}>
+                {/* <div><Navbar /></div> */}
+                <div>{props.childrenProp}</div>
+            </body>
+        </ContextVariables.Provider>
+    )
+}
